2ea6d339ab44ddfa9e9e2ee5517f7d3f46fa3755
nitinsharmacs <ramujha266@gmail.com>
2020-08-17 13:06:36 +0530

    cookie problem

--- routes/auth.js
+++ routes/auth.js
@@ -29,8 +29,13 @@ router.get('/facebook/redirect', (req, res, next)=>{
 
 router.get('/google', passport.authenticate('google-login', {scope:['https://www.googleapis.com/auth/plus.login', 'profile', 'email']}));
 
-router.get('/google/redirect', passport.authenticate('google-login', {session:false,
-		successRedirect:process.env.REDIRECT}) ,controllers.googleLogin);
+router.get('/google/redirect', (req, res, next)=>{
+	passport.authenticate('google-login', {session:false}, (err, user, info)=>{
+		console.log(user)
+		req.user = user;req.err=err;req.info=info;
+		return next();
+	})(req, res, next);
+} ,controllers.googleLogin);
 
 router.get('/twitter', passport.authenticate('twitter-login', {session:false}));
 
