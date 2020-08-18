252bbc0c5c6e9e3fc09d1afa8adc732086b70ef1
nitinsharmacs <ramujha266@gmail.com>
2020-08-18 10:07:49 +0530

    authwork

--- routes/auth.js
+++ routes/auth.js
@@ -3,37 +3,6 @@ const passport = require('passport');
 
 const controllers = require('../controllers/auth');
 
-// router.post('/login', (req, res, next)=>{
-// 	passport.authenticate('local-login', (err, user, info)=>{
-// 		console.log(user);
-// 		console.log(err);
-// 		console.log(info)
-// 		req.logIn(user, err=>{
-// 			console.log('LOGIN')
-// 			return res.status(200).json({message:'success', status:200})
-// 		})
-// 		// if(err)
-// 		// 	return res.status(500).json({message:'Logined failed, try again', status:500});
-// 		// if(!user){
-// 		// 	return res.status(info.status).json({message:info.message, status:info.status});
-// 		// 	req.logIn(user, err=>{
-// 		// 		if(err)
-// 		// 			return res.status(500).json({message:'login failed', status:500});
-// 		// 		return res.status(200).json({message:'succ', status:200})
-// 		// 	})
-// 		// }
-// 	})(req, res, next);
-// });
-
-// router.post('/login', passport.authenticate('local-login'), (req, res, next)=>{
-// 		res.status(200).json({message:'donoe', status:200});
-// 	// console.log(signature.sign(req.sessionID, process.env.JWT_KEY))
-// 	//res.redirect(process.env.REDIRECT)
-// })
-
-//router.post('/login', passport.authenticate('local-login'),);
-
-
 router.post('/login', (req, res, next)=>{
 	passport.authenticate('local-login', (err, user, info)=>{
 		req.user = user; req.err = err;req.info = info;
@@ -41,7 +10,6 @@ router.post('/login', (req, res, next)=>{
 	})(req, res, next);
 }, controllers.localLogin);
 
-// router.post('/login', passport.authenticate('local-login'), controllers.localLogin);
 
 router.post('/register', (req, res, next) => {
 	passport.authenticate('local-register', (err, user, info)=>{
